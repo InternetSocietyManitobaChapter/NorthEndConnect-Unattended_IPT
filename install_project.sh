fdbd598754cbc7da7ad0bdc6a0079d373c0b64e3
Joel Templeman <59157736+JoelTempleman@users.noreply.github.com>
2022-04-03 11:31:41 -0500

    Create install_project.sh
    draft

---
+++ install_project.sh
@@ -0,0 +1,4 @@
+# A simple script to 
+sudo apt update
+sudo apt install docker-compose -y   # This will install all the requirements to run Docker VMs.
+sudo docker-compose up -d    # The -d is for "detached" so they will run in the background and still allow use of the root OS.
