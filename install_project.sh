02d2d5b552175d9dca821468a152466dfbc6bf2d
Joel Templeman <59157736+JoelTempleman@users.noreply.github.com>
2022-04-14 18:47:51 -0500

    Update install_project.sh

--- install_project.sh
+++ install_project.sh
@@ -1,3 +1,15 @@
 # A simple script to install everything
 sudo apt update 
 
+# Update package manager (apt-get) 
+# and install (with the yes flag `-y`)
+# Python and Pip
+apt-get update && apt-get install -y \
+    python3.8 \
+    python3-pip
+
+# Install our Python dependencies
+pip install Requests Pygments
+
+# Run the script when the image is run
+# ["python3", "/home/server.py"]
