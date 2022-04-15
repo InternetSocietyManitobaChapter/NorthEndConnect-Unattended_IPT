df04ba8ed7ab3caeeece142207b22072226e74e3
Joel Templeman <59157736+JoelTempleman@users.noreply.github.com>
2022-04-15 17:44:42 -0500

    Update install_project.sh

--- install_project.sh
+++ install_project.sh
@@ -3,12 +3,34 @@
 # Update package manager (apt-get) 
 # and install (with the yes flag `-y`)
 # Python and Pip
-apt-get update && apt-get install -y \
-    python3.8 \
-    python3-pip
+apt-get update && apt-get upgrade && apt autoremove -y
+apt-get install python3.8 -y
+apt-get install python3-pip -y
 
-# Install our Python dependencies
-pip install Requests Pygments
+# Install other Python dependencies
+pip3 install Requests Pygments
+pip3 install selenium
+pip3 install webdriver-manager
+python3 -m pip install -U nuitka
+
+#Compile the "main.py" python code into UPT.exe
+cd client
+python3 -m nuitka --windows-disable-console -o UPT.exe main.py
+
+#ZIP the files and move them to the /server folder
+apt install zip -y
+zip -r UPT.zip *
+cp UPT.zip ../server
+
+#Compile the python code for the Updater.exe
+cd ../updater
+python3 -m nuitka --windows-disable-console -o updater.exe updater.py
+zip -r updater.zip *
+
+#Compile the python code for the "CIRA only" test
+cd ../CIRA
+python3 -m nuitka --windows-disable-console -o cira.exe cira.py
+zip -r cira.zip *
 
 # Run the script when the image is run
 # ["python3", "/home/server.py"]
