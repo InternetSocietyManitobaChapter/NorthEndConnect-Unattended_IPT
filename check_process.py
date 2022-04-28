d09e75a8c0e42650c22347f6e4519cd960787870
Joel Templeman <59157736+JoelTempleman@users.noreply.github.com>
2022-04-28 18:11:33 -0500

    Create check_process.py

---
+++ check_process.py
@@ -0,0 +1,10 @@
+def check_process():
+    import subprocess
+    script_name = "server.py"
+    cmd='pgrep -f .*python.*{}'.format(script_name)
+    process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    my_pid, err = process.communicate()
+    if len(my_pid.splitlines()) >0:
+        print("Script Running in background")
+        sys.exit(0);
+check_process()
