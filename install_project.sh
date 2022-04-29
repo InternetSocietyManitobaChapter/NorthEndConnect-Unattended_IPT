aab8c6ed5f7659b4c59bcab26612183d16e2f119
Joel Templeman <59157736+JoelTempleman@users.noreply.github.com>
2022-04-28 20:34:27 -0500

    Update install_project.sh

--- install_project.sh
+++ install_project.sh
@@ -16,26 +16,25 @@ pip3 install psutil
 pip3 install subprocess.run
 
 #Compile the "main.py" python code into UPT.exe
-cd client
-python3 -m nuitka --windows-disable-console -o UPT.exe main.py
+#cd client
+#python3 -m nuitka --windows-disable-console -o UPT.exe main.py
 
 #ZIP the files and move them to the /server folder
-apt install zip -y
-zip -r UPT.zip *
-cp UPT.zip ../server
+#apt install zip -y
+#zip -r UPT.zip *
+#cp UPT.zip ../server
 
 #Compile the python code for the Updater.exe
-cd ../updater
-python3 -m nuitka --windows-disable-console -o updater.exe updater.py
-zip -r updater.zip *
+#cd ../updater
+#python3 -m nuitka --windows-disable-console -o updater.exe updater.py
+#zip -r updater.zip *
 
 #Compile the python code for the "CIRA only" test
-cd ../CIRA
-python3 -m nuitka --windows-disable-console -o cira.exe cira.py
-zip -r cira.zip *
+#cd ../CIRA
+#python3 -m nuitka --windows-disable-console -o cira.exe cira.py
+#zip -r cira.zip *
 
 # Run the script to start listening for connections
-
 cd ../server
 #chmod +x server.py
 #python3 server.py
