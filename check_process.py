5cc259d98960f8a3c1fef3928a7ec833745aad9f
Joel Templeman <59157736+JoelTempleman@users.noreply.github.com>
2022-04-28 19:00:11 -0500

    Update check_process.py

--- check_process.py
+++ check_process.py
@@ -1,12 +1,12 @@
-def check_process():
-    import subprocess
-    import urllib3
-    script_name = "server.py"
-    cmd='pgrep -f .*python.*{}'.format(script_name)
-    process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    my_pid, err = process.communicate()
-    if len(my_pid.splitlines()) >0:
-        print("Script Running in background")
+#def check_process():
+#    import subprocess
+#   import urllib3
+#    script_name = "server.py"
+#    cmd='pgrep -f .*python.*{}'.format(script_name)
+#    process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+#    my_pid, err = process.communicate()
+#    if len(my_pid.splitlines()) >0:
+#        print("Script Running in background")
         #sys.exit(0);
 #check_process()
 
